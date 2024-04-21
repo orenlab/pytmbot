730c655c71ac549a4e54ba60ac731af52d541bd9
Denis Rozhnovskiy <pytelemonbot@mail.ru>
2024-04-21 18:26:29 +0500

    Added correct error handling when an error occurs when connecting to a Dockers socket

--- app/core/handlers/default_handlers/containers_handler.py
+++ app/core/handlers/default_handlers/containers_handler.py
@@ -4,30 +4,37 @@
 PyTMBot - A simple Telegram bot designed to gather basic information about
 the status of your local servers
 """
+import docker
 
 from app.core.handlers.handler import Handler
 from app import bot_logger
 from telebot.types import Message
 
 from app.core.adapters.docker_adapter import DockerAdapter
+from docker.errors import DockerException
 
 
 class ContainersHandler(Handler):
     def __init__(self, bot):
         """Initialize the ContainersHandler"""
         super().__init__(bot)
+        self.log = bot_logger
         self.docker_adapter = DockerAdapter()
 
     def _get_data(self):
         """Use docker adapter to gather containers information"""
-        data = self.docker_adapter.check_image_details()
-        return data
+        try:
+            data = self.docker_adapter.check_image_details()
+            return data
+        except DockerException:
+            bot_logger.error('Error connecting to the Docker socket')
+            return {}
 
     def _compile_message(self) -> str:
         """Compile the message to be sent to the bot"""
         try:
             context = self._get_data()
-            if context == {}:
+            if context == {} or not context:
                 bot_answer = self.jinja.render_templates(
                     'none.jinja2',
                     thought_balloon=self.get_emoji('thought_balloon')
@@ -50,6 +57,7 @@ class ContainersHandler(Handler):
             Get docker containers info
             """
             try:
+                self.bot.send_chat_action(message.chat.id, 'typing')
                 bot_logger.info(self.bot_msg_tpl.HANDLER_START_TEMPLATE.format(
                     message.from_user.username,
                     message.from_user.id,
@@ -62,6 +70,8 @@ class ContainersHandler(Handler):
                     text=containers_bot_answer
                 )
             except ValueError:
-                raise self.exceptions.PyTeleMonBotHandlerError(
-                    self.bot_msg_tpl.VALUE_ERR_TEMPLATE
+                self.log.error("Error while handling message")
+                self.bot.send_message(
+                    message.chat.id,
+                    text="Error occurred while getting containers info :("
                 )
