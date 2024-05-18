0087206cc7b4ec4b6bfa444e87a054b76b982658
Denis Rozhnovskiy <pytelemonbot@mail.ru>
2024-05-18 17:22:45 +0500

    Change app version to dev

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ from app.core import exceptions
 config = BotSettings()
 
 # Set global name
-__version__ = '0.0.5'
+__version__ = '0.0.6-dev-20240518'
 __author__ = 'Denis Rozhnovskiy <pytelemonbot@mail.ru>'
 __license__ = 'MIT'
 __repository__ = 'https://github.com/orenlab/pytmbot'
