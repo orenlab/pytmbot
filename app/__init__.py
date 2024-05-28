3c74e384b3073606fb80c2d338e5917c1ff5579d
Denis Rozhnovskiy <pytelemonbot@mail.ru>
2024-05-28 18:31:44 +0500

    Add hadolint.yml

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ from app.core.settings.bot_settings import BotSettings
 config = BotSettings()
 
 # Set global name
-__version__ = '0.0.8-dev-20240524'
+__version__ = '0.0.8-dev-20240528'
 __author__ = 'Denis Rozhnovskiy <pytelemonbot@mail.ru>'
 __license__ = 'MIT'
 __repository__ = 'https://github.com/orenlab/pytmbot'
