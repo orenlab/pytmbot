e026744383e9b4171bfb51d79792df08b578d5ce
Denis Rozhnovskiy <roghnovskiy@gmail.com>
2024-06-18 20:41:53 +0500

    Remove unused code

--- app/core/settings/loggers.py
+++ app/core/settings/loggers.py
@@ -11,4 +11,3 @@ class MessageTpl:
     ERROR_USER_BLOCKED_TEMPLATE: str = "You do not have permission to access this service. I apologize."
     VALUE_ERR_TEMPLATE: str = "Invalid message format"
     TPL_ERR_TEMPLATE: str = "Error parsing template"
-    HANDLER_START_TEMPLATE: str = "Start handling session. User: {0}, user_id: {1}, lang: {2}, is_bot: {3}"
