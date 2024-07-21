9d7049e85af27d853db8b316e33310f72354ff1d
Denis Rozhnovskiy <roghnovskiy@gmail.com>
2024-07-21 17:42:03 +0500

    Docker Action hotfix

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ from app.utilities.utilities import parse_cli_args
 config = BotSettings()
 
 # Set global name
-__version__ = 'v0.1.0'
+__version__ = 'v0.1.1'
 __author__ = 'Denis Rozhnovskiy <pytelemonbot@mail.ru>'
 __license__ = 'MIT'
 __repository__ = 'https://github.com/orenlab/pytmbot'
