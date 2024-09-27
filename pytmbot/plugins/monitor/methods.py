29f7ebfd3f9242f084a4b2ece69bfac5c32985ee
Denis Rozhnovskiy <pytelemonbot@mail.ru>
2024-09-27 14:03:59 +0500

    fix: excludes system partitions

--- pytmbot/plugins/monitor/methods.py
+++ pytmbot/plugins/monitor/methods.py
@@ -13,6 +13,8 @@ import importlib.util
 import pygal
 from telebot import TeleBot
 
+from pytmbot.plugins.monitor.models import MonitoringData
+
 # Check if the psutil module is installed
 if importlib.util.find_spec("psutil") is None:
     raise ModuleNotFoundError("psutil library is not installed. Please install it.")
@@ -28,71 +30,6 @@ import threading
 import time
 from datetime import timedelta
 from io import BytesIO
-
-
-class MonitoringData:
-    """Singleton class for storing and managing monitoring data."""
-
-    _instance = None
-
-    def __new__(cls, *args, **kwargs):
-        """Override __new__ to ensure only one instance is created."""
-        if cls._instance is None:
-            cls._instance = super(MonitoringData, cls).__new__(cls)
-            bot_logger.debug("MonitoringData singleton instance created.")
-        return cls._instance
-
-    def __init__(self, retention_days: int = 7):
-        """Initialize the class with a retention period."""
-        # To ensure __init__ is not called multiple times in case of repeated instantiation
-        if not hasattr(self, 'initialized'):  # This is to ensure it's only initialized once
-            self.retention_days = retention_days
-            self.data = {
-                "cpu_usage": deque(),
-                "memory_usage": deque(),
-                "disk_usage": deque(),
-                "temperatures": deque(),
-            }
-            self.stop_event = threading.Event()
-            self.cleaning_thread = threading.Thread(target=self._clean_old_data, daemon=True)
-            self.cleaning_thread.start()
-            self.initialized = True  # Mark as initialized to avoid multiple initializations
-            bot_logger.info(f"MonitoringData initialized with retention period of {self.retention_days} days.")
-
-    def add_data(self, cpu: float, memory: float, disk: float, temperature: float):
-        """Adds new data to the storage."""
-        timestamp = datetime.now()
-        self.data["cpu_usage"].append((timestamp, cpu))
-        self.data["memory_usage"].append((timestamp, memory))
-        self.data["disk_usage"].append((timestamp, disk))
-        self.data["temperatures"].append((timestamp, temperature))
-
-    def _clean_old_data(self):
-        """Removes data older than the retention period."""
-        while not self.stop_event.is_set():
-            self._remove_old_entries()
-            time.sleep(3600)  # Check every hour
-
-    def _remove_old_entries(self):
-        """Removes old entries from the data."""
-        cutoff_time = datetime.now() - timedelta(days=self.retention_days)
-        for key in self.data.keys():
-            old_count = len(self.data[key])
-            while self.data[key] and self.data[key][0][0] < cutoff_time:
-                self.data[key].popleft()
-            if old_count != len(self.data[key]):
-                bot_logger.debug(f"Removed old entries from {key}, new count: {len(self.data[key])}.")
-
-    def stop_cleaning(self):
-        """Stops the background cleaning thread."""
-        self.stop_event.set()
-        self.cleaning_thread.join()
-        bot_logger.debug("Stopped the cleaning thread for MonitoringData.")
-
-    def get_data(self):
-        """Returns the stored data."""
-        bot_logger.debug("Data retrieved from MonitoringData.")
-        return {key: list(value) for key, value in self.data.items()}
 
 
 class SystemMonitorPlugin(PluginCore):
@@ -272,20 +209,27 @@ class SystemMonitorPlugin(PluginCore):
             self.bot_logger.error(f"Error checking memory usage: {e}")
             return 0.0
 
+    import psutil
+
     def _check_disk_usage(self) -> float:
         """
-        Checks the current disk usage for all partitions and sends a notification if any partition's usage exceeds the configured threshold.
+        Checks the current disk usage for all physical partitions and sends a notification if any partition's
+        usage exceeds the configured threshold.
+        Excludes system partitions like loop, tmpfs, and others that are usually used by the system.
         """
+        excluded_fs_types = ["loop", "tmpfs", "devtmpfs", "proc", "sysfs", "cgroup", "mqueue", "hugetlbfs", "overlay",
+                             "aufs"]
+
         total_disk_usage = 0.0
         try:
-            for partition in psutil.disk_partitions():
+            partitions = [p for p in psutil.disk_partitions() if p.fstype not in excluded_fs_types]
+            for partition in partitions:
                 usage = psutil.disk_usage(partition.mountpoint)
                 if usage.percent > self.settings.plugins_config.monitor.tracehold.disk_usage_threshold[0]:
                     self._send_notification(
                         f"{self.config.emoji_for_notification}Disk usage is high on {partition.device}: {usage.percent}%")
                 total_disk_usage += usage.percent
-            return total_disk_usage / len(
-                psutil.disk_partitions()) if psutil.disk_partitions() else 0.0
+            return total_disk_usage / len(partitions) if partitions else 0.0
         except psutil.Error as e:
             self.bot_logger.error(f"Error checking disk usage: {e}")
             return 0.0
