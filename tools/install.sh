75e4a63460aa751a87bb4f2ae77f868eec8bf6c1
Den Rozhnovskiy <pytelemonbot@mail.ru>
2024-12-28 15:39:07 +0500

    feat: bump version to 0.2.2-dev

--- tools/install.sh
+++ tools/install.sh
@@ -69,7 +69,7 @@ show_banner() {
     echo -e "${CYAN}${border_style}"
     echo -e "${WHITE}Starting the ${YELLOW}$(echo "${action}" | tr '[:lower:]' '[:upper:]')${WHITE} process...${NC}"
     echo -e "${CYAN}${border_style}"
-    echo -e "${WHITE}Version: 0.2.1${NC}"
+    echo -e "${WHITE}Version: 0.2.2-dev${NC}"
     echo -e "${WHITE}Follow us on GitHub: https://github.com/orenlab/pytmbot${NC}"
     echo -e "${CYAN}${border_style}"
     echo ""
