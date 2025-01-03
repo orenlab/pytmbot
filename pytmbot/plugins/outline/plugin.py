f2924f5e74200103c0e3c2dfd2590c63033d24e3
Den Rozhnovskiy <pytelemonbot@mail.ru>
2025-01-03 22:12:37 +0500

    fix: remove unexpected keyword argument 'session_type' in method call

--- pytmbot/plugins/outline/plugin.py
+++ pytmbot/plugins/outline/plugin.py
@@ -26,7 +26,7 @@ class OutlinePlugin(PluginInterface):
         super().__init__(bot)
         self.plugin_logger = plugin.logger
 
-    @plugin.logger.session_decorator(session_type="handler")
+    @plugin.logger.session_decorator
     def outline_handler(self, message: Message) -> None:
         """
         Handles the '/outline' command by sending a compiled response
@@ -48,7 +48,7 @@ class OutlinePlugin(PluginInterface):
             message.chat.id, response, reply_markup=keyboard, parse_mode="Markdown"
         )
 
-    @plugin.logger.session_decorator(session_type="handler")
+    @plugin.logger.session_decorator
     def handle_server_info(self, message: Message) -> Message:
         """
         Handles messages with 'Server info' by sending server information
@@ -82,7 +82,7 @@ class OutlinePlugin(PluginInterface):
         )
         self.bot.send_message(message.chat.id, response, parse_mode="HTML")
 
-    @plugin.logger.session_decorator(session_type="handler")
+    @plugin.logger.session_decorator
     def handle_keys(self, message: Message) -> Message:
         """
         Handles messages with 'Keys' by sending key information compiled
@@ -113,7 +113,7 @@ class OutlinePlugin(PluginInterface):
         )
         self.bot.send_message(message.chat.id, response, parse_mode="HTML")
 
-    @plugin.logger.session_decorator(session_type="handler")
+    @plugin.logger.session_decorator
     def handle_traffic(self, message: Message) -> Message:
         """
         Handles messages with 'Traffic' by sending traffic information
@@ -166,8 +166,8 @@ class OutlinePlugin(PluginInterface):
             )
 
     def _get_action_data(
-        self,
-        action: Literal["key_information", "server_information", "traffic_information"],
+            self,
+            action: Literal["key_information", "server_information", "traffic_information"],
     ) -> Optional[Dict]:
         """
         Retrieves action data from the plugin methods and processes it.
@@ -203,11 +203,11 @@ class OutlinePlugin(PluginInterface):
         return None
 
     def _compile_template(
-        self,
-        template_name: str,
-        first_name: str,
-        context: Optional[Dict] = None,
-        **kwargs: dict[str, Any],
+            self,
+            template_name: str,
+            first_name: str,
+            context: Optional[Dict] = None,
+            **kwargs: dict[str, Any],
     ) -> str:
         """
         Compiles the template with the provided context and first name.
@@ -219,11 +219,11 @@ class OutlinePlugin(PluginInterface):
         :return: The compiled template response as a string.
         """
         with Compiler(
-            template_name=template_name,
-            first_name=first_name,
-            set_naturalsize=set_naturalsize,
-            context=context or {},
-            **kwargs,
+                template_name=template_name,
+                first_name=first_name,
+                set_naturalsize=set_naturalsize,
+                context=context or {},
+                **kwargs,
         ) as compiler:
             response = compiler.compile()
 
