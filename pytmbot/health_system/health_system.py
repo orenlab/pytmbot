06b4e741e4a74292d468403b64d5991e342a5970
Den Rozhnovskiy <pytelemonbot@mail.ru>
2025-09-09 11:24:56 +0500

    chore(docs): fix docs

--- pytmbot/health_system/health_system.py
+++ pytmbot/health_system/health_system.py
@@ -1,9 +1,8 @@
 #!/usr/local/bin/python3
 """
-Professional Health System Module
-pytmbot/health_system.py
-
-Fixed version with proper async integration and lifecycle management.
+(c) Copyright 2025, Denis Rozhnovskiy <pytelemonbot@mail.ru>
+pyTMBot - A simple Telegram bot to handle Docker containers and images,
+also providing basic information about the status of local servers.
 """
 
 from __future__ import annotations
